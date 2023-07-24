59b4fd011d4338b70f455797414119af40353523
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-07-24 17:54:23 +0000

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -44,13 +44,13 @@ void selection_sort(int *array, size_t size)
 		min_index = i;
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min_index])
-			{
 				min_index = j;
-				print_array(array, size);
-			}
 
 		/* Swap the found minimum element with the first element */
 		if (min_index != i)
+		{
 			swap(&array[min_index], &array[i]);
+			print_array(array, size);
+		}
 	}
 }
